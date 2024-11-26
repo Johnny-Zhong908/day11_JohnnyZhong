f5f6d36fcc12fbe2709b7df17fe9ce150dd45349
jeany0515 <devxu@thoughtworks.com>
2024-11-26 11:29:06 +0800

    refactor: remove the useless code

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,12 @@
 import TodoGenerator from "./TodoGenerator";
 import TodoGroup from "./TodoGroup";
-import { useState } from "react";
 
 const TodoList = () => {
-  const [todoList, setTodoList] = useState([]);
-
-  const handleAddTodo = (text) => {
-    setTodoList([...todoList, text]);
-  }
-
   return (
       <div>
         <h2>{"Todo List"}</h2>
-        <TodoGroup todoList={todoList}/>
-        <TodoGenerator addTodo={handleAddTodo}/>
+        <TodoGroup/>
+        <TodoGenerator/>
       </div>
   );
 }
