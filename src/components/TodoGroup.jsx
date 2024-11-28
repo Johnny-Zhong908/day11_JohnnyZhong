75ac9744f99372df3c751aceb0fe29337600d316
My Name <myEmail@example.com>
2024-11-28 19:08:16 +0800

    feat:build TodoGroup.jsx.jsx.jsx

--- src/components/TodoGroup.jsx
+++ src/components/TodoGroup.jsx
@@ -1,17 +1,29 @@
-import { useContext } from "react";
-import { TodoListContext } from "../context/TodoListContext";
+import React, { useContext } from "react";
 import TodoItem from "./TodoItem";
+import { TodoContext } from "../context/TodoContext";
+import TodoGenerator from "./TodoGenerator";
+import styles from "./TodoGroup.module.css";
 
 const TodoGroup = () => {
-  const { state } = useContext(TodoListContext)
-  return (
-      <div>
-        {state.map((todo) => {
-          return <TodoItem key={todo.id} todo={todo}/>
-        })}
-      </div>
-  );
-}
+    const { state } = useContext(TodoContext);
+
+    const todoItems = state.map((todo) => (
+        <TodoItem key={todo.id} id={todo.id} text={todo.text} done={todo.done} />
+    ));
+
+    return (
+        <div className={styles.groupContainer}>
+            <h1 className={styles.title}>Todo List</h1>
+            {state.length === 0 ? (
+                <div className={styles.emptyState}>
+                    Add the things you need to do today...
+                </div>
+            ) : (
+                todoItems
+            )}
+            <TodoGenerator />
+        </div>
+    );
+};
 
 export default TodoGroup;
-
