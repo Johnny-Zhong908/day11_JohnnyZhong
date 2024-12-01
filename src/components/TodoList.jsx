496931f4ad09ff6316670ef9f21d7ed052b37b3a
My Name <myEmail@example.com>
2024-12-01 18:01:21 +0800

    feat:add button to axios page

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -7,6 +7,7 @@ import {Navigate} from "react-router-dom";
 const TodoList = () => {
     const [redirectToDone, setRedirectToDone] = useState(false);
     const [redirectToNotFound, setRedirectToNotFound] = useState(false);
+    const [redirectToAxios, setRedirectToAxios] = useState(false);
     if (redirectToDone) {
         return <Navigate to="/done" />;
     }
@@ -14,16 +15,22 @@ const TodoList = () => {
     if (redirectToNotFound) {
         return <Navigate to="/nonexistent-route" />;
     }
+    if (redirectToAxios) {
+        return <Navigate to="/axios" />;
+    }
     return (
         <div className="todo-list">
             <h1>Todo List</h1>
-            <TodoGroup />
-            <TodoGenerator />
+            <TodoGroup/>
+            <TodoGenerator/>
             <p>
-                <button onClick={() => setRedirectToDone(true)}>DoneList Page </button>
+                <button onClick={() => setRedirectToDone(true)}>DoneList Page</button>
             </p>
             <p>
                 <button onClick={() => setRedirectToNotFound(true)}>404 NotFound Page</button>
+            </p>
+            <p>
+                <button onClick={() => setRedirectToAxios(true)}>Axios Todo List Page</button>
             </p>
         </div>
     );
