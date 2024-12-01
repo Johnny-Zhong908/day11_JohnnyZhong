95e543efb6185c3b0afe610a3372a43e61661535
My Name <myEmail@example.com>
2024-12-01 17:21:23 +0800

    feat:add GET operation

--- src/axios/TodoList.jsx
+++ src/axios/TodoList.jsx
@@ -1,9 +1,17 @@
 import {useState} from "react";
+import axios from "axios";
 
 const TodoList = ()=>{
     const [todos, setTodos] = useState([]);
     const [newTodo, setNewTodo] = useState('');
     const [error, setError] = useState(null);
-
     const API_URL = 'https://67495c87868020296630ab65.mockapi.io/todo/TodoItems';
+    const fetchTodos = async () => {
+        try {
+            const response = await axios.get(API_URL);
+            setTodos(response.data);
+        } catch (err) {
+            setError(err.message);
+        }
+    };
 }
