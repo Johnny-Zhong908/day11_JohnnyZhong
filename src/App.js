6b2e8b073321f0edaed7bb1865845b4c1e95ecbc
My Name <myEmail@example.com>
2024-12-01 16:28:26 +0800

    feat:add Navigate function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TodoList from "./components/TodoList";
-import { TodoProvider } from "./components/TodoContext";
+import DoneList from "./components/DoneList";
 import NotFound from "./components/NotFound";
-import DoneList from "./components/DoneList"; // Import the 404 page
+import { TodoProvider } from "./components/TodoContext";
 
 const App = () => {
     return (
@@ -11,7 +11,7 @@ const App = () => {
             <TodoProvider>
                 <Routes>
                     <Route path="/" element={<TodoList />} />
-                    <Route path="/done" element={<DoneList/>} />
+                    <Route path="/done" element={<DoneList />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </TodoProvider>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import TodoGroup from "./TodoGroup";
 import TodoGenerator from "./TodoGenerator";
 import "./Todo.css";
 import {Link} from "react-router-dom";
+import {Navigate} from "react-router-dom";
 
 const TodoList = () => {
+    const [redirectToDone, setRedirectToDone] = useState(false);
+    const [redirectToNotFound, setRedirectToNotFound] = useState(false);
+    if (redirectToDone) {
+        return <Navigate to="/done" />;
+    }
+
+    if (redirectToNotFound) {
+        return <Navigate to="/nonexistent-route" />;
+    }
     return (
         <div className="todo-list">
             <h1>Todo List</h1>
-            <TodoGroup/>
-            <TodoGenerator/>
+            <TodoGroup />
+            <TodoGenerator />
             <p>
-                <Link to="/nonexistent-route">Go to 404 Page</Link>
+                <button onClick={() => setRedirectToDone(true)}>DoneList Page </button>
             </p>
             <p>
-                <Link to="/done">Go to Done List</Link>
+                <button onClick={() => setRedirectToNotFound(true)}>404 NotFound Page</button>
             </p>
         </div>
     );
